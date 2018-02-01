e2e0c83a6f14c61c7a8f60292f532540a323b899
mattstancer <mfjpoland@gmail.com>
2018-02-01 11:46:24 +0100

    smtp client

---
+++ Pop3 Client/Pop3 Client/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Timers;
+using System.Net.Mail;
+using System.Net;
+namespace Pop3_Client
+{
+    public partial class Form1 : Form
+    {
+        Pop3 pop3c;
+        System.Timers.Timer myTimer=new System.Timers.Timer();
+        SmtpClient c = new SmtpClient();
+        public Form1()
+        {
+            InitializeComponent();
+            pop3c= new Pop3(995, "poczta.interia.pl");
+            c.EnableSsl = true;
+            c.Host = "smtp.gmail.com";
+            c.Credentials = new NetworkCredential("jakis.mailzut@gmail.com","zutsieci");
+            c.Port = 587;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            myTimer.Elapsed += new ElapsedEventHandler(DisplayTimeEvent);
+            myTimer.Interval = 10000; // 1000 ms is one second
+        }
+
+       
+        string[] lines;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Regex r = new Regex("OK (?<mess>[0-9]+) (?<id>[0-9]+)");
+
+            lines = Regex.Split(pop3c.login(textBox1.Text, textBox2.Text), "\r\n");
+            foreach (string line in lines)
+            {
+
+                Match tcpline = r.Match(line);
+                if (tcpline.Success)
+                {
+                    int messagesnumber = Convert.ToInt16(tcpline.Groups["mess"].Value);
+
+                        oldmessagesno = messagesnumber;
+
+                }
+            }
+            myTimer.Start();
+            
+        }
+
+        int oldmessagesno = 0;
+        public void DisplayTimeEvent(object source, ElapsedEventArgs e)
+        {
+            Regex r = new Regex("OK (?<mess>[0-9]+) (?<id>[0-9]+)");
+
+            lines = Regex.Split(pop3c.login(textBox1.Text, textBox2.Text), "\r\n");
+            foreach (string line in lines)
+            {
+
+                Match tcpline = r.Match(line);
+                if (tcpline.Success)
+                {
+                   int messagesnumber = Convert.ToInt16(tcpline.Groups["mess"].Value);
+                    if (oldmessagesno < messagesnumber) {
+                        int roznica = messagesnumber- oldmessagesno;
+                        oldmessagesno = messagesnumber;
+                       string[] newlines = Regex.Split(pop3c.checkHeaders(roznica, messagesnumber,textBox1.Text, textBox2.Text), "\r\n");
+                        Regex r2 = new Regex("From: <(?<email>.*)>");
+                        foreach (string newline in newlines)
+                        {
+                            Match mailline = r2.Match(newline);
+                            if (mailline.Success)
+                            { MessageBox.Show(mailline.Groups["email"].Value); }
+                        }
+                        }
+                }
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = Encoding.Default.GetBytes("proba");
+            string message = Encoding.UTF8.GetString(bytes);
+            bytes = Encoding.Default.GetBytes("proba");
+            string title = Encoding.UTF8.GetString(bytes);
+            c.Send("jakis.mailzut@gmail.com", textBox1.Text, title, message);
+        }
+    }
+}
---
+++ Pop3 Client/Pop3 Client/Pop3.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Cryptography;
+
+using System.IO;
+namespace Pop3_Client
+{
+    class Pop3
+    {
+        public TcpClient cli;
+        private StreamWriter sw;
+        private System.IO.StreamReader reader;
+        private int port=110;
+        private string host = "pop.gmail.com";
+    
+        public Pop3(int port, string host) {
+            
+            this.port = port;
+            this.host = host;
+            
+        }
+        public string clientRun() {
+            this.cli = new TcpClient();
+            this.cli.Connect(this.host, this.port);
+            byte[] bytesToSend = System.Text.Encoding.ASCII.GetBytes("STAT");
+            System.Net.Security.SslStream sslstream = new SslStream(this.cli.GetStream());
+            sslstream.AuthenticateAsClient(this.host);
+             sw = new StreamWriter(sslstream);
+             reader = new StreamReader(sslstream);
+            sw.WriteLine("STAT");
+            sw.Flush();
+            string str= reader.ReadLine();
+            return str;
+            this.cli.GetStream().Close();
+            this.cli.Close();
+        }
+        public string stopClient() {
+         
+            this.cli = new TcpClient();
+            this.cli.Connect(this.host, this.port);
+            System.Net.Security.SslStream sslstream = new SslStream(this.cli.GetStream());
+            sslstream.AuthenticateAsClient(this.host);
+            sw = new StreamWriter(sslstream);
+            reader = new StreamReader(sslstream);
+            sw.WriteLine("QUIT");
+            sw.Flush();
+            string str = reader.ReadLine();
+            this.cli.GetStream().Close();
+            this.cli.Close();
+            
+            return str;
+        }
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+        public string login(string email, string password) {
+            //password = GetMd5Hash(MD5.Create(), password);
+            this.cli = new TcpClient();
+            this.cli.Connect(this.host, this.port);
+            System.Net.Security.SslStream sslstream = new SslStream(this.cli.GetStream());
+            sslstream.AuthenticateAsClient(this.host);
+            StreamWriter sw = new StreamWriter(sslstream);
+            System.IO.StreamReader reader = new StreamReader(sslstream);
+            sw.WriteLine("USER " + email); sw.Flush();
+            sw.WriteLine("PASS "+password); sw.Flush();
+            sw.WriteLine("STAT");
+            sw.Flush();
+            sw.WriteLine("Quit ");
+            sw.Flush();
+            string str = string.Empty;
+            string strTemp = string.Empty;
+            while ((strTemp = reader.ReadLine()) != null)
+            {
+                if (".".Equals(strTemp))
+                {
+                    break;
+                }
+                if (strTemp.IndexOf("-ERR") != -1)
+                {
+                    break;
+                }
+                str += strTemp+"\r\n";
+            }
+         
+            string ret = str;
+            return ret;
+        }
+        public string checkHeaders(int roznica, int messages,string email, string password)
+        {
+            //password = GetMd5Hash(MD5.Create(), password);
+            this.cli = new TcpClient();
+            this.cli.Connect(this.host, this.port);
+            System.Net.Security.SslStream sslstream = new SslStream(this.cli.GetStream());
+            sslstream.AuthenticateAsClient(this.host);
+            StreamWriter sw = new StreamWriter(sslstream);
+            System.IO.StreamReader reader = new StreamReader(sslstream);
+            sw.WriteLine("USER " + email); sw.Flush();
+            sw.WriteLine("PASS " + password); sw.Flush();
+            for (int i = (messages-roznica)+1; i <= messages; i++) {
+                sw.WriteLine("TOP "+i + " 0");
+                sw.Flush();
+            }
+            sw.WriteLine("Quit ");
+            sw.Flush();
+            string str = string.Empty;
+            string strTemp = string.Empty;
+            while ((strTemp = reader.ReadLine()) != null)
+            {
+                if (".".Equals(strTemp))
+                {
+                    break;
+                }
+                if (strTemp.IndexOf("-ERR") != -1)
+                {
+                    break;
+                }
+                str += strTemp + "\r\n";
+            }
+
+            string ret = str;
+            return ret;
+        }
+    }
+
+}
